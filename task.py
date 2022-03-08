5406d5e8702734429307126feb0bd0ca72dcae74
“Yuseph <yusephs@hotmail.com>
2022-03-08 16:29:18 +0000

    YS

--- task.py
+++ task.py
@@ -1,6 +1,22 @@
 import csv
+import pandas as pd
 
-with open ('data2.csv', 'r') as file:
-    data = csv.reader(file, delimiter = '|')
+df = pd.read_csv('data2.csv', sep = '|')
+#compare the average view count for 2 genres
 
-    print(data)
+#genre_1 =input('input 1st genre')
+g1cf =df[(df['genres']=='Comedy')&(df['tomatometer_status']=='Certified-Fresh')]
+g1cfl =len(g1cf)
+g1 =df[(df['genres']=='Comedy')]
+g1l =len(g1)
+average =100*float(g1cfl)/float(g1l)
+print('percentage of cf in comedy is: '& 100*float(g1cfl)/float(g1l))
+
+#g1l =len(g1)
+#g1vc = 
+
+
+#genre_2 =input('input 2nd genre')
+#day =df[(df['genre']==genre_2)]
+
+#average_g1 =
