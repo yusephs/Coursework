c3683acc18e6acda1aec6c25835a3991ecbe5093
“Yuseph <yusephs@hotmail.com>
2022-03-09 10:20:41 +0000

    YS

--- task.py
+++ task.py
@@ -4,9 +4,9 @@ import pandas as pd
 df = pd.read_csv('data2.csv', sep = '|')
 
 
-a = input("What is the first genre you would like to compare\n")
+a = input("What is the first genre you would like to compare\n").title()
 print("")
-b = input("What is the second genre you would like to compare\n")
+b = input("What is the second genre you would like to compare\n").title()
 
 
 acf =len(df[(df['genres']==a)&(df['tomatometer_status']=='Certified-Fresh')])
@@ -22,8 +22,17 @@ bg =len(df[(df['genres']==b)])
 
 pacf = 100*acf/ag
 paf = 100*af/ag
-par = 100*ar/12
+par = 100*ar/ag
 
 pbcf = 100*bcf/bg
 pbf = 100*bf/bg
-pbr = 100*br/12
+pbr = 100*br/bg
+
+
+print("percentage of certified fresh is: " + str(pacf))
+print("percentage of fresh is: " + str(paf))
+print("percentage of rotten is: " + str(par) + "\n")
+
+print("percentage of certified fresh is: " + str(pbcf))
+print("percentage of fresh is: " + str(pbf))
+print("percentage of rotten is: " + str(pbr))
